81289ec00bae2630ed55d4937393ad9e923072ee
Andrey Shlapkov <a1@indigobunting.com>
2018-01-22 14:18:21 +0300

    Implemented text mesh pro UGUI localize

---
+++ Assets/IndieLocalization/Scripts/Stub/TextMeshProUGUI.cs
@@ -0,0 +1,10 @@
+﻿namespace IndigoBunting.Lang.Stub
+{
+    /// <summary>
+    /// This class is a stub
+    /// </summary>
+    public class TextMeshProUGUI
+    {
+        public void SetText(string text) {  }
+    }
+}
---
+++ Assets/IndieLocalization/Scripts/UI/UILangTextMeshProUGUI.cs
@@ -0,0 +1,37 @@
+﻿//using TMPro;
+using IndigoBunting.Lang.Stub; //remove this line if you use TextMeshPro
+using UnityEngine;
+
+namespace IndigoBunting.Lang
+{
+    public class UILangTextMeshProUGUI : MonoBehaviour
+    {
+        [SerializeField]
+        private string key;
+        
+        private TextMeshProUGUI text;
+
+        private void Awake()
+        {
+            text = GetComponent<TextMeshProUGUI>();
+            SetLocalizedText(Localizer.Instance.Language);
+        }
+
+        private void Start()
+        {
+            Localizer.OnChangedLanguage += SetLocalizedText;
+        }
+
+        private void OnDestroy()
+        {
+            Localizer.OnChangedLanguage -= SetLocalizedText;
+        }
+
+        private void SetLocalizedText(LangCode code)
+        {
+            text.SetText(Localizer.Instance.GetText(key));
+        }
+    }
+}
+
+
